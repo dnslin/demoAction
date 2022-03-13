5e3c66e93eaec71ed041246078e7716aa19919f2
dnslin <dnslint@gmail.com>
2022-03-13 13:25:26 +0800

    更新启动脚本🤷‍

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker rm -f demoAction
 docker pull dnslin/demoaction:latest
 docker rm -f demoaction||true&&docker run  --name=demoAction -d -p 6789:6789 dnslin/demoaction:latest
 docker image prune -af
