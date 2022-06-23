055f4ab82b48ea4824ea0386787437410c10c0fb
dnslin <dnslint@gmail.com>
2022-06-23 12:26:21 +0800

    增加arm构建🤣

--- src/test/java/com/dnslin/demoaction/DemoActionApplicationTests.java
+++ src/test/java/com/dnslin/demoaction/DemoActionApplicationTests.java
@@ -1,13 +1,37 @@
 package com.dnslin.demoaction;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import utils.MailClient;
+
+import javax.annotation.Resource;
+import javax.mail.internet.MimeMessage;
+import java.util.Map;
 
 @SpringBootTest
-class DemoActionApplicationTests {
+public class DemoActionApplicationTests {
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
+    @Value("${spring.mail.username}")
+    private String username;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("wjy");
+        message.setText("核酸做不完");
+        message.setTo("i@zr.ci"); //收件人邮箱地址,请自行修改
+        message.setFrom(username);
+        mailSender.send(message);
     }
-
 }
