8701a3555a06dc5afd9e55ba8598f601071e272f
dnslin <dnslint@gmail.com>
2022-06-23 13:07:19 +0800

    测试tag

--- src/main/java/com/dnslin/demoaction/controller/Action.java
+++ src/main/java/com/dnslin/demoaction/controller/Action.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Action {
 
     @GetMapping("/action")
-
     public String action(){
         return "访问action成功";
     }
